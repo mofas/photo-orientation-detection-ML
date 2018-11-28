e41bf1496f8282a965ce38b8b71ed2b69a037bd7
mofas223 <mofas223@gmail.com>
2018-11-28 16:49:22 -0500

    update report
    Signed-off-by: mofas223 <mofas223@gmail.com>

--- TODO.md
+++ TODO.md
@@ -18,21 +18,6 @@ time ./orient.py test test-data.txt forest_model.txt forest
 
 ### Which classifiers and which parameters would you recommend to a potential client?
 
-### How does performance vary depending on the training dataset size,
-
 ### Show a few sample images that were classified correctly and incorrectly.
-
-# read args from argv
-
-Test data
-nearest: 0.6260416666666667
-adaboost: 0.6927
-forest: 0.7302
-
-adaboost:
-forest:
-best THRESHOLD: 180
-
-# train model based on the real input
 
 best_model.txt.
--- orient.py
+++ orient.py
@@ -6,40 +6,39 @@ import pickle
 import random
 import numpy as np
 
-#### Report
 # Nearest model:
 # Nearest model is very simple, we just look through all the images,
 # and I use "vector_diff" function to find the accumulated square vector difference between
-# two images in all 192 pxiels. The only parameter we need to decide is how many "closest"
-# images (NEAREST_K) to choose to vote the final result.
+# two images in all 192 pixels. The only parameter we need to decide is how many "closest"
+# images (NEAREST_K) to choose to vote for the final result.
 # I try K = 5, K = 10, k = 20, and k = 50
 # K = 5  : 0.5979
 # K = 10 : 0.6177
 # K = 50 : 0.6260
 # K = 100: 0.6333
 #
-# However, the performance of nearest model decrease significantly if we don't have enough
+# However, the performance of the nearest model decrease significantly if we don't have enough
 # data. For example, if we only have 1000 training image, the accuracy is lower than 0.3,
 # which is pretty close random guess.
 #
-# Another drawback of nearest model is it's classify time take too long,
+# Another drawback of the nearest model is its classified time take too long,
 # because we actually compared the test image to all the training data.
 #
-# In short, nearest k model performace well when you have lots of data. However,
-# the extremely long classify time make this algorithm impractical.
+# In short, nearest k model performed well when you have lots of data. However,
+# the extremely long classify time to make this algorithm impractical.
 #
 #
 # #
 # Adaboost model:
-# In adaboost model, I iterate all the possible weak classifier candidates (192*192)
+# In AdaBoost model, I iterate all the possible weak classifier candidates (192*192)
 # and choose the best n classifier (NUM_ADABOOST_CLASSIFIER) to form the
-# final adaboost classifier. In addition, I find the lots of candidates are very
-# good at label "0" images. While only few candidates can do good jobs at label "270".
+# final AdaBoost classifier. In addition, I find the lots of candidates are very
+# good at label "0" images. While only a few candidates can do good jobs at label "270".
 #
 # Therefore, I choose the best NUM_ADABOOST_CLASSIFIER/4 candidates for all orientations.
-# I belive through this mechanism, I can find the "experts" for all orientations.
+# I believe through this mechanism, I can find the "experts" for all orientations.
 #
-# The following is the accuracy for differnt NUM_ADABOOST_CLASSIFIER.
+# The following is the accuracy for different NUM_ADABOOST_CLASSIFIER.
 #
 # NUM_ADABOOST_CLASSIFIER = 2   : 0.2646
 # NUM_ADABOOST_CLASSIFIER = 5   : 0.659375
@@ -55,45 +54,68 @@ import numpy as np
 # I think I can explain that if there are too many too weak classifiers can vote,
 # then it will create lots of noise and decrease the performance.
 #
-# The classify time of adaboost is blazing fast, because we just summary the
+# The classify time of AdaBoost is blazing fast because we just summary the
 # result of all classifiers.
 #
-# Another benefit of adaboost is it works quite well even we don't have lots of data.
-# For 1000 data, the performance of adaboost can over 0.4, which is pretty good
+# Another benefit of AdaBoost is it works quite well even we don't have lots of data.
+# For 1000 data, the performance of AdaBoost can over 0.4, which is pretty good
 # compared with the nearest K.
 #
-# However, the training time of adaboost is a drawback, for training around 40k
+# However, the training time of AdaBoost is a drawback, for training around 40k
 # data, it will take around 400 sec in my computer to train, which is much slower
 # than forest model.
 #
 # #
 # Forest model:
-# Forest model is quite complex to implmenet because lots of decisions to make
+# Forest model is quite complex to implement because lots of decisions to make
 # and parameters to tune.
 # In my implementation, I use the value of a pixel as features to split dataset.
 # If the value of certain pixel higher than parameter THRESHOLD, then
-# we put them into left subtree, otherwise, in right subtree.
+# we put them into the left subtree, otherwise, in the right subtree.
 #
 # In addition, when we split the data, which index to use is also important.
-# In theory, we can try all 192 index and choose the one giving us the lowest entropy.
-# However, I find if I random choose some good indexes but not the best.
+# In theory, we can try all 192 indexes and choose the one giving us the lowest entropy.
+# However, I find if I randomly choose some good indexes but not the best.
 # I will get the better performance in the end!
 #
-# This is a amazing result. Therefore, I have an parameter called "SPLIT_SAMPLING"
+# This is an amazing result. Therefore, I have a parameter called "SPLIT_SAMPLING"
 # to decide how many samplings we did for finding a good index.
-# #
-# Other parameter is how many tree we should have and how depth each tree will be.
-# It is very hard to tune. Neverthelee, both of them should depend on how many
+#
+# Another experiment I try it how many data per tree. Notice, if the number of tree
+# multiple by the number of data per tree larger than the total data we have,
+# we will need to "reuse" some data. I do the several experiments and I find if
+# a data is reused in more than 2 trees, the performance will decrease
+# gradually.
+#
+# Other parameter is how many trees we should have and how depth each tree will be.
+# It is very hard to tune. Nevertheless, both of them should depend on how many
 # train data you have. From my personal experience, if we give a tree too many data,
 # then it will grow very high(deep), which caused the overfitting. If we give too few
-# data, then tree is underfitting. Similarity, growing too many trees or too few
+# data, then the tree is underfitting. Similarity, growing too many trees or too few
 # trees will decrease performance.
 #
-# I try the several parameter, and I find the optimal tree depth is around 14 ~ 18,
+# I try the several parameters, and I find the optimal tree depth is around 14 ~ 18,
 # and the optimal tree number is around 300 ~ 600. The optimal threshold for
-# splitting data is around 150 ~ 200.  The number of sampling splited index is
-# around 60.
+# splitting data is around 150 ~ 200. The number of data feeds to each tree is
+# around 200. (Well, the data "reuse rate" rate based on above parameter
+# is 2, which mean a data will be used by 2 trees).
+# The number of sampling split index is around 60.
 #
+# The forest model has several advantages. First of all, high performance compared to
+# AdaBoost and nearest K algorithm. Secondly, relatively faster training time compared to
+# AdaBoost, which help developers to tune parameters easily. Thirdly, the classified time is
+# also very fast, in just several seconds. Fourth, forest model doing quite well even
+# we don't have lots of data. For 1000 data, the accuracy of forest model can over 0.7
+# if we tune the parameter properly. Compared with 0.3 of nearest K, and 0.45 of AdaBoost,
+# the performance is unbelievably good.
+#
+#
+#
+# #
+# Summary:
+# Overall, I will recommend the forest model to a potential client to use.
+# It is fast and easy to train. It also has the best performance among
+# these 3 models. Moreover, it performs well even we don't have lots of data.
 #
 #
 # #
@@ -104,6 +126,12 @@ import numpy as np
 # Adaboost       0.6927          400s                 0.1s
 # Forest         0.7479          20s                  1s
 # Best
+#
+# Sample image classified correctly and incorrectly
+#
+#
+#
+#
 #
 # #
 #### End Report
